3657f519522f9f42c8972ef69f8f15fcd28d0269
Akio Jinsenji <akio-jinsenji@cloud-creative-studios.com>
2022-03-28 21:16:01 +0900

    P12 とプロビジョニングプロファイルの Base64 を指定する方式も追加

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,11 @@ import * as core from '@actions/core'
 import * as exec from '@actions/exec'
 import * as io from '@actions/io'
 import * as os from 'os'
+import * as tmp from 'tmp'
+import * as fs from 'fs/promises'
+import * as path from 'path'
 import { BooleanStateValue, StringStateValue } from './StateHelper'
+import { KeychainFile } from '@akiojin/keychain'
 
 const IsMacOS = os.platform() === 'darwin'
 
@@ -26,45 +30,72 @@ function MatchProvisioningProfile(text: string, name: string, type: string): str
 	return match.join('\n').split('|')[3].trim()
 }
 
+async function DoFastlane()
+{
+	process.env.MATCH_APP_IDENTIFIER = core.getInput('app-identifier')
+	process.env.MATCH_TYPE = core.getInput('type')
+	process.env.FASTLANE_TEAM_ID = core.getInput('team-id')
+	process.env.MATCH_GIT_URL = core.getInput('git-url')
+	process.env.MATCH_PASSWORD = core.getInput('git-passphrase')
+	process.env.MATCH_GIT_BRANCH = core.getInput('git-branch')
+	process.env.MATCH_KEYCHAIN_NAME = core.getInput('keychain')
+	process.env.MATCH_KEYCHAIN_PASSWORD = core.getInput('keychain-password')
+	process.env.MATCH_READONLY = 'true'
+
+	let output: string = ''
+	const options: exec.ExecOptions = {
+		listeners: {
+			stdout: (data: Buffer) => {
+				output += data.toString()
+			}
+		}
+	}
+
+	core.startGroup('Run fastlane "match"')
+	await exec.exec('fastlane', ['match'], options)
+	core.endGroup()
+
+	const UUID = MatchProvisioningProfile(output, process.env.MATCH_APP_IDENTIFIER, 'UUID')
+	const name = MatchProvisioningProfile(output, process.env.MATCH_APP_IDENTIFIER, 'Name')
+	const path = MatchProvisioningProfile(output, process.env.MATCH_APP_IDENTIFIER, 'Path')
+
+	core.setOutput('provisioning-profile', path)
+	core.setOutput('provisioning-profile-uuid', UUID)
+	core.setOutput('provisioning-profile-name', name)
+
+	core.info(`UUID: ${UUID}`)
+	core.info(`Name: ${name}`)
+	core.info(`Path: ${path}`)
+
+	ProvisioningProfile.Set(path)
+}
+
+async function DoSelfSigning()
+{
+	const provisioning = tmp.tmpNameSync()
+	await fs.writeFile(provisioning, Buffer.from(core.getInput('provisioning-profile-base64'), 'base64'))
+
+	const installed = `${process.env.HOME}/Library/MobileDevice/Provisioning Profiles/${path.basename(provisioning)}.mobileprovision`
+	await io.mv(provisioning, installed)
+
+	core.setOutput('provisioning-profile', installed)
+	ProvisioningProfile.Set(installed)
+
+	const certificate = tmp.tmpNameSync() + '.p12'
+	await fs.writeFile(certificate, Buffer.from(core.getInput('p12-base64'), 'base64'))
+
+	var keychain = await KeychainFile.Open(core.getInput('keychain'), core.getInput('keychain-password'))
+	await keychain.ImportCertificateFromFile(certificate, core.getInput('p12-password'))
+}
+
 async function Run()
 {
 	try {
-		process.env.MATCH_APP_IDENTIFIER = core.getInput('app-identifier')
-		process.env.MATCH_TYPE = core.getInput('type').replace('-', '')
-		process.env.FASTLANE_TEAM_ID = core.getInput('team-id')
-		process.env.MATCH_GIT_URL = core.getInput('git-url')
-		process.env.MATCH_PASSWORD = core.getInput('git-passphrase')
-		process.env.MATCH_GIT_BRANCH = core.getInput('git-branch')
-		process.env.MATCH_KEYCHAIN_NAME = core.getInput('keychain')
-		process.env.MATCH_KEYCHAIN_PASSWORD = core.getInput('keychain-password')
-		process.env.MATCH_READONLY = 'true'
-
-		let output: string = ''
-		const options: exec.ExecOptions = {
-			listeners: {
-				stdout: (data: Buffer) => {
-					output += data.toString()
-				}
-			}
+		if (!!core.getInput('p12-base64')) {
+			await DoSelfSigning()
+		} else {
+			await DoFastlane()
 		}
-
-		core.startGroup('Run fastlane "match"')
-		await exec.exec('fastlane', ['match'], options)
-		core.endGroup()
-
-		const provisioningProfileUUID = MatchProvisioningProfile(output, process.env.MATCH_APP_IDENTIFIER, 'UUID')
-		const provisioningProfileName = MatchProvisioningProfile(output, process.env.MATCH_APP_IDENTIFIER, 'Name')
-		const provisioningProfilePath = MatchProvisioningProfile(output, process.env.MATCH_APP_IDENTIFIER, 'Path')
-
-		core.setOutput('provisioning-profile', provisioningProfilePath)
-		core.setOutput('provisioning-profile-uuid', provisioningProfileUUID)
-		core.setOutput('provisioning-profile-name', provisioningProfileName)
-
-		core.info(`UUID: ${provisioningProfileUUID}`)
-		core.info(`Name: ${provisioningProfileName}`)
-		core.info(`Path: ${provisioningProfilePath}`)
-
-		ProvisioningProfile.Set(provisioningProfilePath)
 	} catch (ex: any) {
 		core.setFailed(ex.message)
 	}
